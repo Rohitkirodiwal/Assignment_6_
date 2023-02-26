e73859459d7ea101365666e6e32084765627b635
Rohitkirodiwal <121967233+Rohitkirodiwal@users.noreply.github.com>
2023-02-26 22:57:47 +0530

    Add files via upload

---
+++ _assignment_2_.py
@@ -0,0 +1,8 @@
+states = {"Names":[{"Rajasthan": "Jaipur"},
+                   {"haryana": "Chandigrh"},
+                   {"UP":"Lucknow"},
+                   {"Punjab":"chandigarh"},
+                   {"Delhi":"is a capital"},
+                   {"MP":"Bhopal"}]}
+print(states)
+import json
